2e1a238fe9baf2e494667fbc49431cc4ba537e0e
Pascal COMBES <pascom@orange.fr>
2014-09-02 21:41:55 +0200

    Added the functionnality of the power manager:
    	-Lock screen.
    	-Close session.
    	-Reboot.
    	-Shutdown.

--- Power/Manager.cs
+++ Power/Manager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace GlobalHotKeys
 {
@@ -20,11 +21,13 @@ namespace GlobalHotKeys
                 if (args.Count > 1)
                     throw new Shortcuts.BadArgumentCountException("shutdown(when) admits 1 optional argument", 0, 1);
                 
-                int? when = null;
+                int when = 60;
                 if (args.Count == 1)
                     when = int.Parse(args[0]);
 
-                Console.WriteLine("Called Power.Manager.shutdown(\"{0}\")", when);
+                Console.WriteLine("Called Power.Manager.shutdown({0})", when);
+
+                Process.Start("shutdown", "/s /t " + when);
             }
 
             static public void reboot(List<string> args)
@@ -32,27 +35,33 @@ namespace GlobalHotKeys
                 if (args.Count > 1)
                     throw new Shortcuts.BadArgumentCountException("reboot(when) admits 1 optional argument", 0, 1);
 
-                int? when = null;
+                int when = 60;
                 if (args.Count == 1)
                     when = int.Parse(args[0]);
 
-                Console.WriteLine("Called Power.Manager.reboot(\"{0}\")", when);
+                Console.WriteLine("Called Power.Manager.reboot({0})", when);
+
+                Process.Start("shutdown", "/r /t " + when);
             }
 
             static public void logOff(List<string> args)
             {
-                if (args.Count > 1)
-                    throw new Shortcuts.BadArgumentCountException("logOff() admits no arguments", 0);
+                if (args.Count != 0)
+                    throw new Shortcuts.BadArgumentCountException("logOff() admits no argument", 0);
 
                 Console.WriteLine("Called Power.Manager.logOff()");
+
+                Process.Start("shutdown", "/l");
             }
 
             static public void lockScreen(List<string> args)
             {
-                if (args.Count > 1)
+                if (args.Count != 0)
                     throw new Shortcuts.BadArgumentCountException("lockScreen() admits no arguments", 0);
 
                 Console.WriteLine("Called Power.Manager.lockScreen()");
+
+                User32.LockWorkStation();
             }
         }
     }
---
+++ Power/User32.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GlobalHotKeys
+{
+    namespace Power
+    {
+        class User32
+        {
+            [DllImport("user32")]
+            internal static extern void LockWorkStation();
+        }
+    }
+}
