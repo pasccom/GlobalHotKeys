e0f1f0b6ace8a06efa2cd05b9f6eaf4f8d2f9ef6
Pascal COMBES <pascom@orange.fr>
2014-09-09 21:57:16 +0200

    Added code to start processes when activating them.
    A working directory can be given as 3rd argument of activate or 2nd
    argument of start.

--- Windows/Manager.cs
+++ Windows/Manager.cs
@@ -14,25 +14,65 @@ namespace GlobalHotKeys
             {
                 get
                 {
-                    return new List<string>() { "activate", "start" };
+                    return new List<string>() { "activate", "start", "focus" };
                 }
             }
 
             static public void activate(List<string> args)
             {
-                if ((args.Count < 1) || (args.Count > 2))
-                    throw new Shortcuts.BadArgumentCountException("activate(when) admits 1 optional argument", 1, 2);
+                if ((args.Count < 1) || (args.Count > 3))
+                    throw new Shortcuts.BadArgumentCountException("activate(exePath, [title, [startPath]]) needs 1 argument at least and admits 2 optional arguments.", 1, 3);
+
+                string exePath = args[0];
+                string title = null;
+                string startPath = null;
+                if (args.Count >= 2)
+                    title = args[1];
+                if (args.Count >= 3)
+                    startPath = args[2];
+
+                Console.WriteLine("Called Windows.Manager.activate(\"{0}\", \"{1}\", \"{2}\")", exePath, title, startPath);
+
+                List<Process> processes = findProcesses(exePath);
+                if (processes.Count == 0) {
+                    // Start process
+                    startProcess(exePath, startPath);
+                } else {
+                    if (title == null) {
+                        // Use the .NET provided MainWindowHandle (not always good...)
+                        activate(processes[0].MainWindowHandle);
+                    } else {
+                        // Look for a visible window whose title matches the provided regexp
+                        List<IntPtr> windows = findVisibleWindows(processes[0].Threads, title);
+                        if (windows.Count == 0)
+                            Console.WriteLine("Warning: No matching windows", windows.Count - 1);
+                        else
+                            activate(windows[0]);
+
+                        if (windows.Count > 1)
+                            Console.WriteLine("Warning: {0} matching windows ignored", windows.Count - 1);
+                    }
+                }
+
+                if (processes.Count > 1)
+                    Console.WriteLine("Warning: {0} matching processes ignored", processes.Count - 1);
+            }
+
+            static public void focus(List<string> args)
+            {
+                if ((args.Count < 1) || (args.Count > 3))
+                    throw new Shortcuts.BadArgumentCountException("focus(exePath, [title]) needs 1 argument at least and admits 1 optional argument.", 1, 2);
 
                 string exePath = args[0];
                 string title = null;
                 if (args.Count == 2)
                     title = args[1];
 
-                Console.WriteLine("Called Windows.Manager.activate(\"{0}\", \"{1}\")", exePath, title);
+                Console.WriteLine("Called Windows.Manager.focus(\"{0}\", \"{1}\")", exePath, title);
 
                 List<Process> processes = findProcesses(exePath);
                 if (processes.Count == 0) {
-                    // Start process...
+                    Console.WriteLine("Warning: No matching process");
                 } else {
                     if (title == null) {
                         // Use the .NET provided MainWindowHandle (not always good...)
@@ -57,17 +97,34 @@ namespace GlobalHotKeys
             static public void start(List<string> args)
             {
                 if (args.Count != 1)
-                    throw new Shortcuts.BadArgumentCountException("start(when) needs 1 argument", 1, 1);
+                    throw new Shortcuts.BadArgumentCountException("start(exePath, [startPath]) needs 1 argument and admits 1 optional argument.", 1, 2);
 
                 string exePath = args[0];
+                string startPath = null;
+                if (args.Count >= 2)
+                    startPath = args[1];
 
-                Console.WriteLine("Called Windows.Manager.activate(\"{0}\")", exePath);
+                Console.WriteLine("Called Windows.Manager.start(\"{0}\")", exePath);
+
+                startProcess(exePath, startPath);
             }
 
             static private void activate(IntPtr winHandle)
             {
-                User32.ShowWindow(winHandle, User32.SW_SHOW);
+                User32.ShowWindow(winHandle, User32.SW_SHOWDEFAULT);
                 User32.SetForegroundWindow(winHandle);
+            }
+
+            static private void startProcess(string exePath, string startPath = null)
+            {
+                Console.WriteLine("Starting " + exePath);
+
+                Process process = new Process();
+                process.StartInfo.FileName = exePath;
+                process.StartInfo.WorkingDirectory = startPath;
+                process.StartInfo.UseShellExecute = false;
+
+                process.Start();
             }
 
             static private List<Process> findProcesses(string exePath)
