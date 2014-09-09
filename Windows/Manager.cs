2a92a88bd1332eab5e07d20c16403f2e4222ba9c
Pascal COMBES <pascom@orange.fr>
2014-09-09 21:13:59 +0200

    Implemented activation of windows.
    There are 2 policies to find a window:
    	-Use the MainWindowHandle provided by .NET. But this is not always
    	accurates (indeed it matches the first visible window). Used if no
    	regexp title is provided.
    	-Use the first window with matching title. This is quite accurate
    	provided a good regular expression is chosen.

--- Windows/Manager.cs
+++ Windows/Manager.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace GlobalHotKeys
 {
@@ -26,6 +29,29 @@ namespace GlobalHotKeys
                     title = args[1];
 
                 Console.WriteLine("Called Windows.Manager.activate(\"{0}\", \"{1}\")", exePath, title);
+
+                List<Process> processes = findProcesses(exePath);
+                if (processes.Count == 0) {
+                    // Start process...
+                } else {
+                    if (title == null) {
+                        // Use the .NET provided MainWindowHandle (not always good...)
+                        activate(processes[0].MainWindowHandle);
+                    } else {
+                        // Look for a visible window whose title matches the provided regexp
+                        List<IntPtr> windows = findVisibleWindows(processes[0].Threads, title);
+                        if (windows.Count == 0)
+                            Console.WriteLine("Warning: No matching windows", windows.Count - 1);
+                        else
+                            activate(windows[0]);
+
+                        if (windows.Count > 1)
+                            Console.WriteLine("Warning: {0} matching windows ignored", windows.Count - 1);
+                    }
+                }
+
+                if (processes.Count > 1)
+                    Console.WriteLine("Warning: {0} matching processes ignored", processes.Count - 1);
             }
 
             static public void start(List<string> args)
@@ -36,6 +62,76 @@ namespace GlobalHotKeys
                 string exePath = args[0];
 
                 Console.WriteLine("Called Windows.Manager.activate(\"{0}\")", exePath);
+            }
+
+            static private void activate(IntPtr winHandle)
+            {
+                User32.ShowWindow(winHandle, User32.SW_SHOW);
+                User32.SetForegroundWindow(winHandle);
+            }
+
+            static private List<Process> findProcesses(string exePath)
+            {
+                Process[] processes = Process.GetProcesses();
+                List<Process> processesMatch = new List<Process>();
+
+                foreach (Process p in processes) {
+                    try {
+                        if (p.Modules[0].FileName == exePath)
+                            processesMatch.Add(p);
+                    } catch (System.ComponentModel.Win32Exception) { }
+                }
+
+                return processesMatch;
+            }
+
+            static private List<IntPtr> findVisibleWindows(ProcessThreadCollection threads, string title)
+            {
+                List<IntPtr> ret = new List<IntPtr>();
+
+                foreach (ProcessThread t in threads)
+                    User32.EnumThreadWindows(t.Id, (IntPtr winHandle, IntPtr param) =>
+                    {
+                        if (!User32.IsWindowVisible(winHandle))
+                            return true;
+
+                        // Gets the window title:
+                        int len1 = User32.GetWindowTextLength(winHandle);
+                        StringBuilder buffer = new StringBuilder(len1 + 1);
+                        int len2 = User32.GetWindowText(winHandle, buffer, len1 + 1);
+                        if (len1 != len2)
+                            throw new ApplicationException("Lengths should match (" + len1 + " != " + len2 + ")"); 
+
+                        // Check if it matches:
+                        Regex regexp = new Regex(title);
+                        if (regexp.IsMatch(buffer.ToString()))
+                            ret.Add(winHandle);
+
+                        return true;
+                    }, IntPtr.Zero);
+
+                return ret;
+            }
+
+            static private void listVisibleWindowsTitle(ProcessThreadCollection threads, string title)
+            {
+                foreach (ProcessThread t in threads)
+                    User32.EnumThreadWindows(t.Id, (IntPtr winHandle, IntPtr param) =>
+                    {
+                        if (!User32.IsWindowVisible(winHandle))
+                            return true;
+
+                        // Gets the window title:
+                        int len1 = User32.GetWindowTextLength(winHandle);
+                        StringBuilder buffer = new StringBuilder(len1 + 1);
+                        int len2 = User32.GetWindowText(winHandle, buffer, len1 + 1);
+                        if (len1 != len2)
+                            throw new ApplicationException("Lengths should match (" + len1 + " != " + len2 + ")"); 
+
+                        Console.WriteLine(buffer);
+
+                        return true;
+                    }, IntPtr.Zero);
             }
         }
     }
