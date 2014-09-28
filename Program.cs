cf961c8267f80971e70e50e933e18f7bb9d598bd
Pascal COMBES <pascom@orange.fr>
2014-09-28 09:03:23 +0200

    Added code to set the log level using command line.
    Depending on the command line loggers are acrivated or deactivated.

--- Program.cs
+++ Program.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Repository.Hierarchy;
 
 namespace GlobalHotKeys
 {
     class Program
     {
-        static private readonly ILog log = LogManager.GetLogger(typeof(Program));
+        static private ILog log;
 
-        static void Start(string[] args)
+        static void Start(int argStart, string[] args)
         {
             string configPath;
             string processesPath;
 
-            if (args.Length > 1)
-                configPath = args[0];
+            if (args.Length > argStart)
+                configPath = args[argStart];
             else
                 configPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Pascom\GlobalHotKeys\globalhotkeys.conf";
 
-            if (args.Length > 2)
-                processesPath = args[1];
+            if (args.Length > argStart + 1)
+                processesPath = args[argStart + 1];
             else
                 processesPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Pascom\GlobalHotKeys\globalhotkeys.processes.conf";
 
@@ -34,12 +38,74 @@ namespace GlobalHotKeys
             app.exec();
         }
 
+        private static void setQuiet()
+        {
+            Hierarchy repo = LogManager.GetRepository() as Hierarchy;
+            if (repo == null)
+                return;
+
+            repo.Root.RemoveAppender("Console");
+
+            foreach (ILogger iLogger in repo.GetCurrentLoggers()) {
+                Logger logger = iLogger as Logger;
+                if (logger != null)
+                    logger.RemoveAppender("Console");
+            }
+        }
+
+        private static void removeFileAppenders(Logger logger)
+        {
+            if (logger == null)
+                return;
+
+            AppenderCollection appenders = logger.Appenders;
+            for (int i = 0; i < appenders.Count; i++)
+                if ((appenders[i].Name != "WindowsEventLog") && (appenders[i].Name != "Console"))
+                    logger.RemoveAppender(appenders[i]);
+        }
+
+        private static void setRelease()
+        {
+            Hierarchy repo = LogManager.GetRepository() as Hierarchy;
+            if (repo == null)
+                return;
+
+            removeFileAppenders(repo.Root);
+            foreach (ILogger iLogger in LogManager.GetRepository().GetCurrentLoggers())
+                removeFileAppenders(iLogger as Logger);
+        }
+
         static void Main(string[] args)
         {
             log4net.Config.XmlConfigurator.Configure();
 
+            int processed = 0;
+            bool debuging = false;
+            bool quiet = false;
+
+            while ((processed < args.Length) && args[processed].StartsWith("--")) {
+                switch (args[processed]) {
+                case "--quiet":
+                    quiet = true;
+                    break;
+                case "--debug":
+                    debuging = true;
+                    break;
+                default:
+                    log.Warn("Unknown argument: " + args[processed]);
+                    break;
+                }
+                processed++;
+            }
+
+            if (quiet)
+                setQuiet();
+            if (!debuging)
+                setRelease();
+            log = LogManager.GetLogger(typeof(Program));
+
             try {
-                Start(args);
+                Start(processed, args);
             } catch (Exception e) {
                 log.Fatal("Unhandled exception caught by main handler", e);
             }
