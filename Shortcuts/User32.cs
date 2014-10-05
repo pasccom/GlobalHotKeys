864de3350a5bdf50fce2a21e5ac8125a4ebd175d
Pascal COMBES <pascom@orange.fr>
2014-10-05 17:51:05 +0200

    Added needed code to register hooks.

--- Shortcuts/User32.cs
+++ Shortcuts/User32.cs
@@ -8,7 +8,16 @@ namespace GlobalHotKeys
         class User32
         {
             internal static readonly uint WM_HOTKEY = 0x0312; /*!< Hotkey message identifier */
-            internal static readonly int MOD_NOREPEAT = 0x4000; /*!< Changes the hotkey behavior so that the keyboard auto-repeat does not yield multiple hotkey notifications. */
+            internal static readonly IntPtr SUCCESS = new IntPtr(1);
+
+            internal struct KeyboardLowLevelHookStruct
+            {
+                public uint vkCode;
+                public uint scanCode;
+                public KeyboardLowLevelHookFlags flags;
+                public uint time;
+                public UIntPtr dwExtraInfo;
+            }
 
             internal struct Point
             {
@@ -26,12 +35,62 @@ namespace GlobalHotKeys
                 public Point pt;
             }
 
-            [DllImport("user32.dll")]
-            internal static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
-            [DllImport("user32.dll")]
-            internal static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+            internal enum KeyboardLowLevelHookFlags
+            {
+                LLKHF_EXTENDED = 0x01,
+                LLKHF_INJECTED = 0x10,
+                LLKHF_ALTDOWN = 0x20,
+                LLKHF_UP = 0x80,
+            }
+
+            internal enum HookType
+            {
+                WH_JOURNALRECORD = 0,
+                WH_JOURNALPLAYBACK = 1,
+                WH_KEYBOARD = 2,
+                WH_GETMESSAGE = 3,
+                WH_CALLWNDPROC = 4,
+                WH_CBT = 5,
+                WH_SYSMSGFILTER = 6,
+                WH_MOUSE = 7,
+                WH_HARDWARE = 8,
+                WH_DEBUG = 9,
+                WH_SHELL = 10,
+                WH_FOREGROUNDIDLE = 11,
+                WH_CALLWNDPROCRET = 12,
+                WH_KEYBOARD_LL = 13,
+                WH_MOUSE_LL = 14
+            }
+
+            internal enum WindowsMessage
+            {
+                KEYDOWN = 0x0100,
+                KEYUP = 0x0101,
+                SYSKEYDOWN = 0x0104,
+                SYSKEYUP = 0x0105,
+            }
+
             [DllImport("user32.dll")]
             internal static extern bool GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
+
+            [DllImport("user32.dll")]
+            internal static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+            internal delegate IntPtr HookProc(int code, IntPtr wParam, IntPtr lParam);
+
+            internal static KeyboardLowLevelHookStruct getKeyboardLowLevelHookStruct(IntPtr lParam)
+            {
+                return (KeyboardLowLevelHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyboardLowLevelHookStruct));
+            }
+
+            [DllImport("user32.dll")]
+            internal static extern IntPtr SetWindowsHookEx(HookType hookType, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+            [DllImport("user32.dll")]
+            internal static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("user32.dll")]
+            internal static extern bool UnhookWindowsHookEx(IntPtr hhk);
         }
     }
 }
