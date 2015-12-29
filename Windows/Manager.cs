3c57489ee5eaab0d45a6ad48851f3f8afcbd6193
Pascal COMBES <pascom@orange.fr>
2015-12-29 21:47:07 +0100

    Documentation for main class of Windows namespace.

--- Windows/Manager.cs
+++ Windows/Manager.cs
@@ -9,11 +9,54 @@ namespace GlobalHotKeys
 {
     namespace Windows
     {
+        /// <summary>
+        ///     Defines the methods of Windows namespace.
+        /// </summary>
+        /// <para>
+        ///     This is the main class of Windows namespace, which are
+        ///     <list type="bullet">
+        ///         <item><term><see cref="activate"/>: </term><description>Bring foreground or start a process
+        ///             <list type="bullet">
+        ///                 <item><term>SizeFlag: </term><description>One of <c>"Default"</c>, <c>"Normal"</c>, <c>"Maximize"</c> and <c>"ShowMaximized"</c></description></item>
+        ///                 <item><term>Process: </term><description>The name of the process</description></item>
+        ///                 <item><term>Title (opt): </term><description>A regexp which should match the title of the window to activate</description></item>
+        ///             </list>
+        ///         </description></item>
+        ///         <item><term><see cref="start"/>: </term><description>Start a process
+        ///             <list type="bullet">
+        ///                 <item><term>Process: </term><description>The name of the process</description></item>
+        ///             </list>
+        ///         </description></item>
+        ///         <item><term><see cref="focus"/>: </term><description>Bring foreground a process
+        ///             <list type="bullet">
+        ///                 <item><term>SizeFlag: </term><description>One of <c>"Default"</c>, <c>"Normal"</c>, <c>"Maximize"</c> and <c>"ShowMaximized"</c></description></item>
+        ///                 <item><term>Process: </term><description>The name of the process</description></item>
+        ///                 <item><term>Title (opt): </term><description>A regexp which should match the title of the window to activate</description></item>
+        ///             </list>
+        ///         </description></item>
+        ///         <item><term><see cref="sendKeys"/>: </term><description>Send a key sequence to an application
+        ///             <list type="bullet">
+        ///                 <item><term>Keys: </term><description>Key sequence to send to the focussed application</description></item>
+        ///             </list>
+        ///         </description></item>
+        ///     </list>
+        /// </para>
         class Manager
         {
+            /// <summary>
+            ///     Provider of processes.
+            /// </summary>
+            /// <seealso cref="ProcessessList"/>
             static private ProcessesProvider mProcessessList;
-
+            /// <summary>
+            ///     Logger for GlobalHokKeys.
+            /// </summary>
+            /// <remarks>See Apache Log4net documentation for the logging interface.</remarks>
             static private readonly ILog log = LogManager.GetLogger(typeof(Manager));
+
+            /// <summary>
+            ///     Setter for the process provider
+            /// </summary>
             static public ProcessesProvider ProcessessList
             {
                 set
@@ -24,6 +67,10 @@ namespace GlobalHotKeys
                 }
             }
 
+            /// <summary>
+            ///     List of declared methods.
+            /// </summary>
+            /// <remarks>Only these methods should be called externally.</remarks>
             static public List<string> AuthorizedMethods
             {
                 get
@@ -32,6 +79,20 @@ namespace GlobalHotKeys
                 }
             }
 
+            /// <summary>
+            ///     Bring foreground or start a process.
+            /// </summary>
+            /// <para>
+            ///     Arguments
+            ///     <list type="bullet">
+            ///         <item><term>SizeFlag: </term><description>One of <c>"Default"</c>, <c>"Normal"</c>, <c>"Maximize"</c> and <c>"ShowMaximized"</c></description></item>
+            ///         <item><term>Process: </term><description>The name of the process</description></item>
+            ///         <item><term>Title (opt): </term><description>A regexp which should match the title of the window to activate</description></item>
+            ///     </list>
+            /// </para>
+            /// <param name="args">Arguments to the function (see above)</param>
+            /// <seealso cref="start"/>
+            /// <seealso cref="focus"/>
             static public void activate(List<string> args)
             {
                 if ((args.Count < 2) || (args.Count > 3))
@@ -72,6 +133,19 @@ namespace GlobalHotKeys
                     log.WarnFormat("{0} matching processes ignored", processes.Count - 1);
             }
 
+            /// <summary>
+            ///     Bring foreground a process.
+            /// </summary>
+            /// <para>
+            ///     Arguments
+            ///     <list type="bullet">
+            ///         <item><term>SizeFlag: </term><description>One of <c>"Default"</c>, <c>"Normal"</c>, <c>"Maximize"</c> and <c>"ShowMaximized"</c></description></item>
+            ///         <item><term>Process: </term><description>The name of the process</description></item>
+            ///         <item><term>Title (opt): </term><description>A regexp which should match the title of the window to activate</description></item>
+            ///     </list>
+            /// </para>
+            /// <param name="args">Arguments to the function (see above)</param>
+            /// <seealso cref="activate"/>
             static public void focus(List<string> args)
             {
                 if ((args.Count < 2) || (args.Count > 3))
@@ -111,6 +185,17 @@ namespace GlobalHotKeys
                     log.WarnFormat("{0} matching processes ignored", processes.Count - 1);
             }
 
+            /// <summary>
+            ///     Start a process.
+            /// </summary>
+            /// <para>
+            ///     Arguments
+            ///     <list type="bullet">
+            ///         <item><term>Process: </term><description>The name of the process</description></item>
+            ///     </list>
+            /// </para>
+            /// <param name="args">Arguments to the function (see above)</param>
+            /// <seealso cref="activate"/>
             static public void start(List<string> args)
             {
                 if (args.Count != 1)
@@ -123,6 +208,16 @@ namespace GlobalHotKeys
                 startProcess(mProcessessList[processName]);
             }
 
+            /// <summary>
+            ///     Send a key sequence to an application.
+            /// </summary>
+            /// <para>
+            ///     Arguments
+            ///     <list type="bullet">
+            ///         <item><term>Keys: </term><description>Key sequence to send to the focussed application</description></item>
+            ///     </list>
+            /// </para>
+            /// <param name="args">Arguments to the function (see above)</param>
             static public void sendKeys(List<string> args)
             {
                 if (args.Count != 1)
@@ -148,6 +243,11 @@ namespace GlobalHotKeys
                     throw new ApplicationException("Could not send the key to application. Error code: " + User32.GetLastError());
             }
 
+            /// <summary>
+            ///     Parses a size flag.
+            /// </summary>
+            /// <param name="sizeName">The size as a string (either a number or one of  <c>"Default"</c>, <c>"Normal"</c>, <c>"Maximize"</c> and <c>"ShowMaximized"</c></description>)</param>
+            /// <returns>The parsed size flag</returns>
             static private User32.ShowState parseSize(String sizeName)
             {
                 bool bad = sizeName.Contains(",");
@@ -167,6 +267,11 @@ namespace GlobalHotKeys
                 }
             }
 
+            /// <summary>
+            ///     Is the given window minimized.
+            /// </summary>
+            /// <param name="winHandle">A handle to the window</param>
+            /// <returns><c>true</c> if the window is minimized, <c>false</c> otherwise</returns>
             static private bool isMiniMized(IntPtr winHandle)
             {
                 User32.Rect rect = new User32.Rect();
@@ -176,6 +281,11 @@ namespace GlobalHotKeys
                 return ((rect.Top == -32000) && (rect.Left == -32000));
             }
 
+            /// <summary>
+            ///     Activates (bring foreground) the given window.
+            /// </summary>
+            /// <param name="winHandle">A handle to the window</param>
+            /// <param name="sizeFlag">How the window should be shown if minimized</param>
             static private void activate(IntPtr winHandle, User32.ShowState sizeFlag)
             {
                 switch (sizeFlag) {
@@ -196,6 +306,10 @@ namespace GlobalHotKeys
                 }
             }
 
+            /// <summary>
+            ///     Start a process
+            /// </summary>
+            /// <param name="processData">Information about th process to be started</param>
             static private void startProcess(ProcessData processData)
             {
                 log.Debug("Waiting mutex release before process start.");
@@ -214,6 +328,11 @@ namespace GlobalHotKeys
                 process.Start();
             }
 
+            /// <summary>
+            ///     Find the processes associated with an executable path.
+            /// </summary>
+            /// <param name="exePath">A (apparent) path to an executable</param>
+            /// <returns>The list of matching processes (may be empty if no process matches)</returns>
             static private List<Process> findProcesses(string exePath)
             {
                 Process[] processes = Process.GetProcesses();
@@ -231,6 +350,15 @@ namespace GlobalHotKeys
                 return processesMatch;
             }
 
+            /// <summary>
+            ///     Find windows for a thread collection.
+            /// </summary>
+            /// <para>
+            ///     Only the windows matching the given title are returned.
+            /// </para>
+            /// <param name="threads">A collection of threads</param>
+            /// <param name="title">A regexp to select windows according to their title</param>
+            /// <returns></returns>
             static private List<IntPtr> findVisibleWindows(ProcessThreadCollection threads, string title)
             {
                 List<IntPtr> ret = new List<IntPtr>();
@@ -263,7 +391,12 @@ namespace GlobalHotKeys
                 return ret;
             }
 
-            static private void listVisibleWindowsTitle(ProcessThreadCollection threads, string title)
+            /// <summary>
+            ///     List the titles of visible windows to <c>Console</c>.
+            /// </summary>
+            /// <remarks>This function is used to write configuration files.</remarks>
+            /// <param name="threads">A collection of threads</param>
+            static private void listVisibleWindowsTitle(ProcessThreadCollection threads)
             {
                 foreach (ProcessThread t in threads)
                     User32.EnumThreadWindows(t.Id, (IntPtr winHandle, IntPtr param) =>
