92722dc91868c66423d665d9a32560a030243831
Pascal COMBES <pascom@orange.fr>
2015-12-29 22:02:05 +0100

    Completed imports documentation in Windows namespace.

--- Windows/User32.cs
+++ Windows/User32.cs
@@ -126,28 +126,52 @@ namespace GlobalHotKeys
                 internal short highParam;
             }
 
+            /// <summary>
+            ///     Show flags. Used for the <see cref="ShowWindow"/> function.
+            /// </summary>
+            /// <remarks>
+            ///     See the ShowWindow function on MSDN. The documentation was copied from there.
+            /// </remarks>
             internal enum ShowState
             {
-                //Hide = 0, /*!< Hides the window and activates another window. */
-                Normal = 1, /*!< Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time. */
-                //ShowMinimized = 2, /*!< Activates the window and displays it as a minimized window. */
-                ShowMaximized = 3, /*!< Activates the window and displays it as a maximized window. */
-                //ShowNoActivate = 4, /*!< Displays a window in its most recent size and position. This value is similar to SW_SHOWNORMAL, except that the window is not activated. */
-                //Show = 5, /*!< Activates the window and displays it in its current size and position. */
-                //Minimize = 6, /*!< Minimizes the specified window and activates the next top-level window in the Z order. */
-                //ShowMinNoActive = 7, /*!< Displays the window as a minimized window. This value is similar to SW_SHOWMINIMIZED, except the window is not activated. */
-                //ShowNA = 8, /*!< Displays the window in its current size and position. This value is similar to SW_SHOW, except that the window is not activated.*/
-                Restore = 9, /*!< Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when restoring a minimized window. */
-                Maximize = 10 /*!< Maximizes the specified window. */
-                //ShowDefault = 10, /*!< Sets the show state based on the SW_ value specified in the STARTUPINFO structure passed to the CreateProcess function by the program that started the application. */
-                //ShowForceMinimize = 11; /*!< Minimizes a window, even if the thread that owns the window is not responding. This flag should only be used when minimizing windows from a different thread. */
+                ///// <summary>Hides the window and activates another window.</summary>
+                //Hide = 0,
+                /// <summary>Activates and displays a window. </summary>
+                Normal = 1,
+                ///// <summary>Activates the window and displays it as a minimized window.</summary>
+                //ShowMinimized = 2,
+                /// <summary>Activates the window and displays it as a maximized window.</summary>
+                ShowMaximized = 3,
+                ///// <summary>Displays a window in its most recent size and position (but does not activate it).</summary>
+                //ShowNoActivate = 4,
+                ///// <summary>Activates the window and displays it in its current size and position.</summary>
+                //Show = 5,
+                ///// <summary>Minimizes the specified window and activates the next top-level window in the Z order.</summary>
+                //Minimize = 6,
+                ///// <summary>Displays the window as a minimized window (but does not activate it).</summary>
+                //ShowMinNoActive = 7,
+                ///// <summary>Displays the window in its current size and position (but does not activate it).</summary>
+                //ShowNA = 8, 
+                /// <summary>Activates and displays the window.</summary>
+                Restore = 9,
+                /// <summary>Maximizes the specified window.</summary>
+                Maximize = 10,
+                //ShowDefault = 10,
+                ///// <summary>Minimizes a window, even if the thread that owns the window is not responding.</summary>
+                //ShowForceMinimize = 11
             }
 
+            /// <summary>
+            ///     Describes the type of input event.
+            /// </summary>
             internal enum InputType : uint
             {
-                MOUSE = 0, /*!< The event is a mouse event. Use the mi structure of the union. */
-                KEYBOARD = 1, /*!< The event is a keyboard event. Use the ki structure of the union. */
-                HARDWARE = 2 /*!< The event is a hardware event. Use the hi structure of the union. */
+                /// <summary>The event is a mouse event.</summary>
+                MOUSE = 0,
+                /// <summary>The event is a keyboard event.</summary>
+                KEYBOARD = 1,
+                /// <summary>The event is a keyboard event.</summary>
+                HARDWARE = 2
             }
 
             /// <summary>
